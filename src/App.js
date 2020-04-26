d3746edb2bac754bad38323fd162c9bd4bb94fdf
Michael Lavva <2333764+mlavva@users.noreply.github.com>
2020-04-26 16:22:00 -0700

    Finished website

--- src/App.js
+++ src/App.js
@@ -12,21 +12,24 @@ class App extends React.Component {
 
   async componentDidMount() {
     const fetchedData = await fetchData();
-    console.log(fetchedData)
-    this.setState({data: fetchedData})
+    console.log(fetchedData);
+    this.setState({data: fetchedData});
   }
 
   handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
     console.log(country);
+    this.setState({data: fetchedData, country: country});
   }
 
   render() {
-    const {data} = this.state;
+    const {data, country} = this.state;
     return (
     <div className={styles.container}>
       <Cards data ={data}/>
       <CountryPicker handleCountryChange={this.handleCountryChange}/>
-      <Chart />
+      {console.log(country)}
+      <Chart data={data} country={country}/>
     </div>
     )
   }
